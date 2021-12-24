29d0a58ea1b1c0da446c5461cac34c6993c007e0
V. Sai Prakash <vukkumsp@gmail.com>
2021-12-24 16:24:37 +0000

    basic apis added - INPROGRESS;

--- src/api/daybook.js
+++ src/api/daybook.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: "Dummy endpoint"
+          message: "Get Accounts"
         }
       ),
     };
